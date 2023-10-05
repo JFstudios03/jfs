bc154b14ca7813d50f47df8e3ce46b34dba53d6b
Jesuferanmi Agboola <117743728+JFstudios03@users.noreply.github.com>
2023-10-05 10:35:54 +0100

    Updates

---
+++ login.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("loginForm");
+    const usernameInput = document.getElementById("email");
+    const passwordInput = document.getElementById("pswd");
+    const loginError = document.getElementById("loginError");
+
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        // Simulate authentication (replace with actual backend logic)
+        const username = usernameInput.value;
+        const password = passwordInput.value;
+
+        // For demonstration purposes, consider 'username' and 'password' as valid
+        if (username === "" && password === "") {
+            // Failed login
+            loginError.textContent = "Invalid username or password.";
+            loginError.style.color = "rgb(161, 0, 0)";
+            loginError.style.backgroundColor = "white";
+            loginError.style.padding = "15px";
+            loginError.style.width = "60%";
+            loginError.style.borderRadius = "20px";
+            loginError.style.margin = "auto";
+        } else if (username === "" || password === "") {
+            // Failed login
+            loginError.textContent = "Invalid username or password.";
+            loginError.style.color = "rgb(161, 0, 0)";
+            loginError.style.backgroundColor = "white";
+            loginError.style.padding = "15px";
+            loginError.style.width = "60%";
+            loginError.style.borderRadius = "20px";
+            loginError.style.margin = "auto";
+        } else {
+            // Successful login
+            loginError.textContent = "Login successful!";
+            loginError.style.color = "green";
+            // loginError.style.backgroundColor = "white";
+            // loginError.style.padding = "15px";
+            // loginError.style.width = "60%";
+            // loginError.style.borderRadius = "20px";
+            // loginError.style.margin = "auto";
+            form.reset();
+        }
+
+    });
+    usernameInput.addEventListener("input", function () {
+        loginError.textContent = "";
+        loginError.style.backgroundColor = "transparent";
+    });
+    passwordInput.addEventListener("input", function () {
+        loginError.textContent = "";
+        loginError.style.backgroundColor = "transparent";
+    });
+});
